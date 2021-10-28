c9617fe84116373c2d126f1b0c833d01e4e5d98d
bublik77 <andrii.bobrovych@gmail.com>
2021-10-28 14:51:37 +0300

    add file

---
+++ check_miss_producer.py
@@ -0,0 +1,71 @@
+import requests
+import json
+from time import sleep
+#import zmq
+import datetime
+#url = 'http://wax.cryptolions.io/v1/chain/get_info'
+url = 'https://jungle3.cryptolions.io/v1/chain/get_info'
+shedule_url = 'https://jungle3.cryptolions.io/v1/chain/get_producer_schedule'
+resp_shed_url = requests.get(url=shedule_url)
+data_shed = resp_shed_url.json()
+prod_shedule_quee = dict()
+
+for i in range(0,21):
+    print(data_shed['active']['producers'][i]['producer_name'])
+    if data_shed['active']['producers'][i]['producer_name'] not in prod_shedule_quee:
+        prod_shedule_quee[i+1] = prod_shedule_quee['active']['producers'][i]['producer_name']
+print(prod_shedule_quee)
+resp = requests.get(url=url)
+data = resp.json()
+serverVersion = data['server_version_string']
+start_block_num = data['head_block_num']
+current_block_producer = data['head_block_producer']
+blocks_produced = 0
+first_run = 1
+# context = zmq.Context()
+# # send results
+# agent_sender = context.socket(zmq.PUSH)
+# agent_sender.connect("tcp://bp01.cryptolions.io:5558") # send results there
+# result = { 'Agent' : 'check_blocks_missing', 'num' : 'started'}
+# agent_sender.send_json(result)
+print("Starting from: ", start_block_num, current_block_producer)
+#url = 'https://bp.cryptolions.io/v1/chain/get_block'
+url = 'http://jungle3.cryptolions.io/v1/chain/get_block'
+#url = 'http://wax.cryptolions.io/v1/chain/get_block'
+headers = {
+    'accept': "application/json",
+    'content-type': "application/json"
+    }
+while True:
+    start_block_num = start_block_num +1
+    data='{"block_num_or_id" : "'+str(start_block_num)+'"}'
+    response = requests.request("POST", url, headers=headers, data=data)
+    data=response.json()
+    try:
+        producer=data['producer']
+    except KeyError:
+#        print (start_block_num, 'not produced yet, sleeping')
+        sleep (10)
+        start_block_num = start_block_num -1
+        continue
+    new_producers=data['new_producers']
+    if current_block_producer == producer:
+        blocks_produced = blocks_produced +1
+    else:
+#        print (current_block_producer, 'produced ', blocks_produced)
+        if blocks_produced < 12 :
+            print (current_block_producer,' is missing blocks' )
+            currentDT = datetime.datetime.now()
+#            rs = json.loads('{"Service": "BlocksMissing","Producer": "'+current_block_producer+'","IP":"127.0.0.1","Date": "'+str(currentDT)+'","Blockchain": "EOS MainNet", "BlocksProduced":"'+str(blocks_produced)+'",      "BlockNumber":"'+str(start_block_num)+'",  "Status": "Critical"}')
+            rs = json.loads('{"Service": "BlocksMissing","Producer": "'+current_block_producer+'","Server": "'+current_block_producer+'","IP":"127.0.0.1","Date": "'+str(currentDT)+'","Blockchain": "WAX", "BlocksProduced":"'+str(blocks_produced)+'",      "BlockNumber":"'+str(start_block_num)+'",  "Status": "Critical"}')
+            if first_run == 0:
+                print(rs)
+                #agent_sender.send_json(rs)
+            if first_run == 1:
+                first_run = 0
+        current_block_producer = producer
+        blocks_produced = 1
+#print (data)
+#    print (start_block_num, producer, new_producers)
+    if new_producers != None:
+        print ('New producers appeared:', new_producers)
