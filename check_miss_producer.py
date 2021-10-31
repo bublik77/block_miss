83c5dfdff61e913b6e3f7c1435b7eaf61639caef
bublik77 <band@pop-os.localdomain>
2021-10-31 21:12:44 +0200

    add

--- check_miss_producer.py
+++ check_miss_producer.py
@@ -5,6 +5,7 @@ import json
 from time import sleep
 from call import call, envirement_data
 import os
+
 try:
     envirement_data()
 except:
@@ -12,14 +13,15 @@ except:
 
 try:
     cryptolions_producers = ["cryptolions1"]
-    url = 'http://wax.cryptolions.io/v1/chain/get_info' #'https://jungle3.cryptolions.io/v1/chain/get_info' #'http://wax.cryptolions.io/v1/chain/get_info'
-    shedule_url = 'http://wax.cryptolions.io/v1/chain/get_producer_schedule' #'https://jungle3.cryptolions.io/v1/chain/get_producer_schedule'
+    url = 'https://wax.cryptolions.io/v1/chain/get_info' #'https://jungle3.cryptolions.io/v1/chain/get_info' #'http://wax.cryptolions.io/v1/chain/get_info'
+    shedule_url = 'https://wax.cryptolions.io/v1/chain/get_producer_schedule' #'https://jungle3.cryptolions.io/v1/chain/get_producer_schedule'
     resp_shed_url = requests.get(url=shedule_url)
     data_shed = resp_shed_url.json()
     prod_shedule_quee = dict()
     chain = "WAX"
+    max_prod_count= len(data_shed['active']['producers'])
     #print(data_shed)
-    for i in range(0,21):
+    for i in range(0,max_prod_count):
         #print(data_shed['active']['producers'][i]['producer_name'])
         if data_shed['active']['producers'][i]['producer_name'] not in prod_shedule_quee:
             prod_shedule_quee[i+1] = data_shed['active']['producers'][i]['producer_name']
@@ -35,7 +37,7 @@ try:
     netx_producer = prod_shedule_quee[(list(prod_shedule_quee.values()).index(current_block_producer)+1)+1]
     
     print("Starting from: ", start_block_num, current_block_producer)
-    url = 'http://wax.cryptolions.io/v1/chain/get_block' #'http://jungle3.cryptolions.io/v1/chain/get_block' #'https://bp.cryptolions.io/v1/chain/get_block' 
+    url = 'https://wax.cryptolions.io/v1/chain/get_block' #'http://jungle3.cryptolions.io/v1/chain/get_block' #'https://bp.cryptolions.io/v1/chain/get_block' 
 
     headers = {
         'accept': "application/json",
@@ -67,12 +69,12 @@ try:
                     netx_producer = prod_shedule_quee[(list(prod_shedule_quee.values()).index(current_block_producer)+1)+1]
                 #print(netx_producer,"new")
             elif netx_producer in cryptolions_producers:
-                print(netx_producer, "miss round")
+                print(netx_producer, "miss round at", start_block_num)
                 for i in os.environ["NUM_TO"].split(" "):
                     call(i, netx_producer, chain)
                 netx_producer = producer
             else:
-                print(netx_producer, "miss round")
+                print(netx_producer, "miss round at", start_block_num)
                 netx_producer = producer
 except:
     print("fin")
