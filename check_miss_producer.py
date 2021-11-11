924dff4874b41a0abbebe04e6c582d84c4a52efb
bublik77 <andriy.bobrovych@gmail.com>
2021-11-11 18:12:12 +0200

    added functions to new shedule

--- check_miss_producer.py
+++ check_miss_producer.py
@@ -6,38 +6,44 @@ from time import sleep
 from call import call, envirement_data
 import os
 
+chain = "WAX"
+cryptolions_producers = ["cryptolions1"]
+try:
+    url_info = 'https://wax.cryptolions.io/v1/chain/get_info' #'https://jungle3.cryptolions.io/v1/chain/get_info' #'http://wax.cryptolions.io/v1/chain/get_info'
+    shedule_url = 'https://wax.cryptolions.io/v1/chain/get_producer_schedule' #'https://jungle3.cryptolions.io/v1/chain/get_producer_schedule'
+except Exception as ex:
+    print(ex)
+
 try:
     envirement_data()
 except:
     print("can't loda data")
 
-try:
-    cryptolions_producers = ["cryptolions1"]
-    url = 'https://api.ultra.cryptolions.io/v1/chain/get_info' #'https://jungle3.cryptolions.io/v1/chain/get_info' #'http://wax.cryptolions.io/v1/chain/get_info'
-    shedule_url = 'https://api.ultra.cryptolions.io/v1/chain/get_producer_schedule' #'https://jungle3.cryptolions.io/v1/chain/get_producer_schedule'
+def bp_schedule_quee():
     resp_shed_url = requests.get(url=shedule_url)
     data_shed = resp_shed_url.json()
     prod_shedule_quee = dict()
-    chain = "WAX"
     max_prod_count= len(data_shed['active']['producers'])
-    print(data_shed, max_prod_count)
+    #print(data_shed, max_prod_count)
     for i in range(0,max_prod_count):
         #print(data_shed['active']['producers'][i]['producer_name'])
         if data_shed['active']['producers'][i]['producer_name'] not in prod_shedule_quee:
             prod_shedule_quee[i+1] = data_shed['active']['producers'][i]['producer_name']
+    return prod_shedule_quee
 
-    #print(prod_shedule_quee)
-    resp = requests.get(url=url)
+#print(len(bp_schedule_quee()))
+try:
+    resp = requests.get(url=url_info)
     data = resp.json()
     serverVersion = data['server_version_string']
     start_block_num = data['head_block_num']
     current_block_producer = data['head_block_producer']
     blocks_produced = 0
     first_run = 1
-    netx_producer = prod_shedule_quee[(list(prod_shedule_quee.values()).index(current_block_producer)+1)+1]
+    netx_producer = bp_schedule_quee()[(list(bp_schedule_quee().values()).index(current_block_producer)+1)+1]
     
     print("Starting from: ", start_block_num, current_block_producer)
-    url = 'https://api.ultra.cryptolions.io/v1/chain/get_block' #'http://jungle3.cryptolions.io/v1/chain/get_block' #'https://bp.cryptolions.io/v1/chain/get_block' 
+    url = 'https://wax.cryptolions.io/v1/chain/get_block' #'http://jungle3.cryptolions.io/v1/chain/get_block' #'https://bp.cryptolions.io/v1/chain/get_block' 
 
     headers = {
         'accept': "application/json",
@@ -60,13 +66,13 @@ try:
             blocks_produced = blocks_produced +1
         else:
             if producer == netx_producer:
-                # print(producer)
-                # print(netx_producer)
+                #print(producer)
+                #print(netx_producer)
                 current_block_producer = producer
-                if list(prod_shedule_quee.values()).index(current_block_producer) == max_prod_count -1:
-                    netx_producer = prod_shedule_quee[1]
+                if list(bp_schedule_quee().values()).index(current_block_producer) == len(bp_schedule_quee()) -1:
+                    netx_producer = bp_schedule_quee()[1]
                 else:
-                    netx_producer = prod_shedule_quee[(list(prod_shedule_quee.values()).index(current_block_producer)+1)+1]
+                    netx_producer = bp_schedule_quee()[(list(bp_schedule_quee().values()).index(current_block_producer)+1)+1]
                 #print(netx_producer,"new")
             elif netx_producer in cryptolions_producers:
                 print(netx_producer, "miss round at", start_block_num)
